7c75518c28d6bf2d8d9e985649e593e3989c43a0
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-03-25 10:28:41 -0400

    Define tasks model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  // Define get all tasks url endpoint
+  @Get()
+  getAllTaks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+}
